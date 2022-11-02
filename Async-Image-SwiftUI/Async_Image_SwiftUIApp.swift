e805566f4cf6dc2bb098156f129dd992f30cd21b
erika-h7 <erika.herrera1@outlook.com>
2022-11-02 11:55:21 -0700

    Initial Commit

---
+++ Async-Image-SwiftUI/Async_Image_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Image_SwiftUIApp.swift
+//  Async-Image-SwiftUI
+//
+//  Created by Infinity Code on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_Image_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
