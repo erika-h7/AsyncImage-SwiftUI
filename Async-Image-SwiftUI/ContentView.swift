26f15e1825c2e7bb707933bd1e72765159bd0d58
erika-h7 <erika.herrera1@outlook.com>
2022-11-02 12:47:34 -0700

    Created an extension for Image to reuse code, loaded a regular AsyncImage, scaled it and used a placeholder.

--- Async-Image-SwiftUI/ContentView.swift
+++ Async-Image-SwiftUI/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // MARK: - 1. BASIC
+        
+        //AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: - 2. SCALE
+        
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: - 3. PLACEHOLDER
+        
+//        AsyncImage(url: URL(string: imageURL)) { image in
+//            image.imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill").iconModifier()
+//        }
+//        .padding(40)
+        
+        // MARK: - 4. PHASE
+        
+        AsyncImage(url: URL(string: imageURL)) { phase in
+            
         }
-        .padding()
     }
 }
 
